b026d360684109840b08f41f2d70b129c6b295da
Stephen Cooper <Stephen.Cooper@cascadehr.co.uk>
2017-07-22 19:03:33 +0100

    l

--- publish.sh
+++ publish.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-if [ -z "$GH_TOKEN" ]; then
-    echo "You must set the GH_TOKEN environment variable."
-    echo "See README.md for more details."
-    exit 1
-fi
+#if [ -z "$GH_TOKEN" ]; then
+#    echo "You must set the GH_TOKEN environment variable."
+#    echo "See README.md for more details."
+#    exit 1
+#fi
 
+export GH_TOKEN="da1984a5e425987b8e4eac3ad111091f08f9f315"
 # This will build, package and upload the app to GitHub.
-node_modules/.bin/build --win --mac -p always
+node_modules/.bin/build --win -p always
