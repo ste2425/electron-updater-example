6ac0a992c55f7d608b450dc2688faf437457bb3a
Stephen Cooper <Stephen.Cooper@cascadehr.co.uk>
2017-07-22 19:45:11 +0100

    5

--- publish.sh
+++ publish.sh
@@ -6,6 +6,5 @@
 #    exit 1
 #fi
 
-export GH_TOKEN="da1984a5e425987b8e4eac3ad111091f08f9f315"
 # This will build, package and upload the app to GitHub.
-node_modules/.bin/build --win --mac -p always
+node_modules/.bin/build --win -p always
